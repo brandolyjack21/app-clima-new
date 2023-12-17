b2d4092f1e75da347f725dfa0c514867f4400b52
brandoly-jack <143424064+brandoly-jack@users.noreply.github.com>
2023-12-16 22:56:13 -0500

    pagina lo carga integrada lista para comprobar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,49 @@
-import './App.css'
-import NavBar from './components/NavBar'
-import WidgetTemperatura from './components/WidgetTemperatura'
-import DetallesDelClima from './components/DetallesDelClima'
-import BackgroundVideo from './components/BackgroundVideo'
-import LoadingPage from './components/LoadingPage'
-import { useSelector, useDispatch } from 'react-redux'
+import "./App.css";
+import NavBar from "./components/NavBar";
+import WidgetTemperatura from "./components/WidgetTemperatura";
+import DetallesDelClima from "./components/DetallesDelClima";
+import BackgroundVideo from "./components/BackgroundVideo";
+import LoadingPage from "./components/LoadingPage";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 
 function App() {
-
-  const loadedApi = useSelector((state) => state.loadedApi.value)
+  const loadedApi = useSelector((state) => state.loadedApi.value);
   console.log(loadedApi);
+
+  const [pages, setPages] = useState([
+    <LoadingPage />,
+    <NavBar />,
+    <WidgetTemperatura />,
+    <DetallesDelClima />,
+    <BackgroundVideo />,
+  ]);
+
+  setTimeout(() => {
+    setPages([
+      <NavBar />,
+      <WidgetTemperatura />,
+      <DetallesDelClima />,
+      <BackgroundVideo />,
+    ]);
+  }, 5000);
 
   return (
     <>
-    {/*<NavBar/>
-    <WidgetTemperatura/>
-    <DetallesDelClima/>
-  <BackgroundVideo/>*/}
-  <LoadingPage/>
+      <div class="video-background">
+        <video
+          id="video-background"
+          src="https://cdn.pixabay.com/vimeo/408654212/birds-35881.mp4?width=640&hash=2a130df3b6157f7ff0c8202b4ca86f4cdcb03c55"
+          muted
+          autoPlay
+          loop
+        ></video>
+      </div>
+      {pages.map((page) => (
+        <>{page}</>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
